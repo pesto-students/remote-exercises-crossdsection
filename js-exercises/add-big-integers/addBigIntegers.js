647863fe20fbad8eff9c7be9c2adc92566e249d9
Piyush Kumar <chirkut.kr@gmail.com>
2020-05-21 08:46:50 +0530

    Solution Add Big Integers

---
+++ js-exercises/add-big-integers/addBigIntegers.js
@@ -0,0 +1,47 @@
+function generateReverseArray(num) {
+  return num.split('').reverse();
+}
+
+function returnZeroForEmptyString(string) {
+  return (string.length > 0) ? string : 0;
+}
+
+function addDigits(indexToAdd, numberMap, carryForward) {
+  let sum = parseInt(carryForward, 10);
+  for (let index = 0; index < numberMap.length; index++) {
+    if (numberMap[index][indexToAdd] !== undefined) {
+      sum += parseInt(numberMap[index][indexToAdd], 10);
+    }
+  }
+  const sumDigitArray = sum.toString().split('');
+  return [
+    returnZeroForEmptyString(sumDigitArray.pop()),
+    returnZeroForEmptyString(sumDigitArray.join('')),
+  ];
+}
+
+function addBigIntegers(intString) {
+  const numberMap = intString.split('\n');
+  let biggestNumberSize = 0;
+  for (let index = 0; index < numberMap.length; index++) {
+    numberMap[index] = generateReverseArray(numberMap[index]);
+    if (numberMap[index].length > biggestNumberSize) {
+      biggestNumberSize = numberMap[index].length;
+    }
+  }
+  const reverseDigitizedTotal = [];
+  let digit = 0;
+  let carryForward = 0;
+  let indexToAdd = 0;
+  while (indexToAdd < biggestNumberSize) {
+    [digit, carryForward] = addDigits(indexToAdd, numberMap, carryForward);
+    reverseDigitizedTotal.push(digit);
+    indexToAdd += 1;
+  }
+  if (carryForward > 0) {
+    reverseDigitizedTotal.push(carryForward);
+  }
+  return reverseDigitizedTotal.reverse().join('');
+}
+
+export { addBigIntegers };
