4af42d2b8854efebccddf8099a1ec5068dacec26
Piyush Kumar <chirkut.kr@gmail.com>
2020-05-22 07:53:26 +0530

    Clean code corrections

--- js-exercises/longest-word-in-string/longestWordInString.js
+++ js-exercises/longest-word-in-string/longestWordInString.js
@@ -1,17 +1,12 @@
 function longestWordInString(sentence) {
   let longestWord = '';
   if (sentence != null) {
-    const alphabets = sentence.split('');
-    const delimeters = [' ', '\t', '\n', ',', '.']; // To break words
+    const words = sentence.split(/\s|\n|,|\./);
     let wordLength = 0;
-    for (let index = 0, startIndex = 0; index < alphabets.length; index++) {
-      if (delimeters.indexOf(alphabets[index]) !== -1 || alphabets[index + 1] == null) {
-        const word = sentence.substring(startIndex, index).trim();
-        if (word.length > wordLength) {
-          longestWord = word;
-          wordLength = word.length;
-        }
-        startIndex = index + 1;
+    for (const word of words) {
+      if (word.length > wordLength) {
+        longestWord = word;
+        wordLength = word.length;
       }
     }
   }
