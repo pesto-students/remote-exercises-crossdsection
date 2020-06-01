10e81502c6d8c951495f43519c3c52dd06976f25
Piyush Kumar <chirkut.kr@gmail.com>
2020-06-01 07:53:16 +0530

    Solution UserCheckPassword

---
+++ js-exercises/user-check-password/user.js
@@ -0,0 +1,21 @@
+
+// (*)
+// Follow the instructions and fill in the blank sections.
+function User(username, password) {
+  this.username = username;
+  this.password = password;
+}
+
+// create a method on User called `checkPassword`
+// this method should take in a string and compare it to the object's password property
+// return `true` if they match, otherwise return `false`
+User.prototype.checkPassword = function checkPassword(password) {
+  if (this.password === password) {
+    return true;
+  }
+  return false;
+};
+
+export {
+  User,
+};
